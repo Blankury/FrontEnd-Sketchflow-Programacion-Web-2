26eceb8ed2889c3c51b02282f8559055e3ee7cac
ivanrdz2001 <ivan.rodriguezm@uanl.edu.mx>
2023-03-22 14:34:57 -0600

    radio buttons edit profile
    ando batallando con el boton para subir cosas

--- src/views/UploadImage.jsx
+++ src/views/UploadImage.jsx
@@ -4,18 +4,27 @@ import sketchflow from "../assets/images/sketchflow.png";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
+
+
+
 export function UploadImage() {
-  const [nameArtwork, setName] = useState("");
-  const [descArtwork, setDesc] = useState("");
-  const [priceArtwork, setPrice] = useState("");
+  
+const campos = {
+  restriccion:false,
+  visibilidad:false
+}
 
+const [nameArtwork, setName] = useState("");
+const [descArtwork, setDesc] = useState("");
+const [priceArtwork, setPrice] = useState("");
 
-  return (
+return (  
     <section className="colorbox  py-5" >
       <div className=" px-5 bg-dark h-50 text-white row d-flex align-items-center justify-content-center">
         <div className=" py-5 text-white text-center">
-          <input className=" redbutton" type="file" id="formFile" />
+          <input className=" redbutton" type="file" id="formFile" onChange={(e)=>this.handleFile(e)}/>
           <hr />
+
           <p>JPEG / GIF / PNG</p>
           <p>
             You can upload up to 32 MB per file and a maximum of 200 files (the total file size must be less than 200 MB)</p>
@@ -26,7 +35,7 @@ export function UploadImage() {
 
       <div className="gradient-custom-2 container colorPrimaryvariant  ">
         <form>
-          <label for="artwork_name" className="formulario__label">artwork_name de la ilustración</label>
+          <label for="artwork_name" className="formulario__label">Nombre de la publicación</label>
           <input type="text" className="form-custom-control" name="artwork_name" id="artwork_name" placeholder="artwork_name de la ilustración" 
           required value={nameArtwork} onChange={(e) => {
             setName(e.target.value);
@@ -34,7 +43,7 @@ export function UploadImage() {
           />
           <label for="artwork_caption" className="formulario__label">Descripción</label>
           <div className="form-group-input">
-            <input type="email" className="form-custom-control" name="artwork_caption" id="artwork_caption" placeholder="artwork_caption" 
+            <input type="text" className="form-custom-control" name="artwork_caption" id="artwork_caption" placeholder="artwork_caption" 
                             required value={descArtwork} onChange={(e) => {
                               setDesc(e.target.value);  }}         
             />
@@ -43,14 +52,14 @@ export function UploadImage() {
             <label >Restricción de edad</label>
             <div className="input-block">
               <label className="rad-label">
-                <input type="radio" className="rad-input " name="rad" id="Todos" value="Todos"
+                <input type="radio" className="rad-input " name="rad" onClick={validarRestriccion()}  id="Todos" value="Todos"
 
                                />
                 <div className="rad-design "></div>
                 <div className="rad-text"> Todos </div>
               </label>
               <label className="rad-label">
-                <input type="radio" className="rad-input gender" name="rad" id="R-18" value="R-18"
+                <input type="radio" className="rad-input gender" name="rad" onClick={validarRestriccion()} id="R-18" value="R-18"
 
                                                />
                 <div className="rad-design"></div>
@@ -60,18 +69,18 @@ export function UploadImage() {
               <label >Visibilidad</label>
               <div className="input-block">
                 <label className="rad-label">
-                  <input type="radio" className="rad-input " name="rad" id="Publico" value="Publico"
+                  <input type="radio" className="rad-input " name="visibilidad" onClick={validarVisibilidad()} id="Publico" value="Publico"
  />
                   <div className="rad-design "></div>
                   <div className="rad-text"> Publico </div>
                 </label>
                 <label className="rad-label">
-                  <input type="radio" className="rad-input " name="rad" id="Privado" value="Privado" />
+                  <input type="radio" className="rad-input " name="visibilidad" onClick={validarVisibilidad()}  id="Privado" value="Privado" />
                   <div className="rad-design "></div>
                   <div className="rad-text"> Privado </div>
                 </label>
                 <label className="rad-label">
-                  <input type="radio" className="rad-input gender" name="rad" id="Pago" value="Pago" />
+                  <input type="radio" className="rad-input gender" name="visibilidad" onClick={validarVisibilidad()}  id="Pago" value="Pago" />
                   <div className="rad-design"></div>
                   <div className="rad-text">De pago </div>
                 </label>
@@ -85,7 +94,7 @@ export function UploadImage() {
             </div>
             <Link to="/Artwork" className="px-3 ">
               <div className="form-group">
-                <button type="submit" className="redbutton form-custom-control submit px-3">Publicar</button>
+                <button type="submit"  className="redbutton form-custom-control submit px-3">Publicar</button>
               </div> </Link>
           </div>
         </form>
@@ -94,5 +103,29 @@ export function UploadImage() {
 
 
   );
-}
 
+  async function validarRestriccion(){
+    if(document.getElementById("Todos").checked === false && document.getElementById("R-18").checked === false ){
+      campos['restriccion']=false;
+      console.log('fake R');
+    }else{
+      campos['restriccion']=true;
+      console.log('TRUE R');
+
+    }
+  }
+
+  async function validarVisibilidad(){
+    if(document.getElementById("Publico").checked === false && document.getElementById("Privado").checked === false && document.getElementById("Pago")===false){
+      campos['visibilidad']=false;
+      console.log('fake V');
+
+    }else{
+      campos['visibilidad']=true;
+      console.log('TRUE V');
+
+    }
+  }
+
+
+}
