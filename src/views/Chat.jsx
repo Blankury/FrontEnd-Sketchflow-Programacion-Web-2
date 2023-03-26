7ac4dc10297ed8f50ff615dfcdf1ddedfc914661
ivanrdz2001 <ivan.rodriguezm@uanl.edu.mx>
2023-03-26 01:13:21 -0600

    chat añadido

--- src/views/Chat.jsx
+++ src/views/Chat.jsx
@@ -1,8 +1,21 @@
 import isotipo from "../assets/images/isotipo.png";
 import logo from "../assets/images/sketchflow_logo.png";
 import sketchflow from "../assets/images/sketchflow.png";
+import ReactDOM from 'react-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
+
+import { useState } from "react";
 
 export function Chat() {
+
+  const [messageText, setMessage] = useState("");
+  const [searchText, setSearch] = useState("");
+  const [files, setFiles] = useState([]);
+
+  const sendIcon = <FontAwesomeIcon icon={faPaperPlane} />
+
   return (
 
     <section className="chatcont">
@@ -13,7 +26,9 @@ export function Chat() {
         </div>
         <div class="srch_bar">
           <div class="stylish-input-group">
-            <input type="text" class="search-bar" placeholder="Search" />
+            <input type="text" class="search-bar" placeholder="Search" 
+                      required value={searchText} onChange={(e) => {
+                        setSearch(e.target.value);  }}  />
             <span class="input-group-addon">
               <button type="button"> <i class="fa fa-search" aria-hidden="true"></i> </button>
             </span> </div>
@@ -38,26 +53,7 @@ export function Chat() {
             </div>
           </div>
         </div>
-        <div class="chat_list">
-          <div class="chat_people">
-            <div class="chat_img"> <img src={logo} class="pfp" alt="User" /> </div>
-            <div class="chat_ib">
-              <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-              <p>Test, which is a new approach to have all solutions
-                    astrology under one roof.</p>
-            </div>
-          </div>
-        </div>
-        <div class="chat_list">
-          <div class="chat_people">
-            <div class="chat_img"> <img src={logo} class="pfp" alt="User" /> </div>
-            <div class="chat_ib">
-              <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-              <p>Test, which is a new approach to have all solutions
-                    astrology under one roof.</p>
-            </div>
-          </div>
-        </div>
+
       </div>
 
       <div className="msger py-3">
@@ -112,8 +108,19 @@ export function Chat() {
         </main>
 
         <form className="msger-inputarea">
-          <input type="text" className="msger-input" placeholder="Enter your message..." />
-          <button type="submit" className="msger-send-btn">Send</button>
+        <div class="image-upload">
+            <label for="file-input">
+
+            </label>
+
+            <input id="file-input" type="file"
+                    multiple
+                    onChange={(event) => setFiles(event.target.files)}/>
+        </div>
+          <input type="text" className="msger-input" placeholder="Escribe tu mensaje"                             
+          required value={messageText} onChange={(e) => {
+                              setMessage(e.target.value);  }}        />
+          <button type="submit" className="msger-send-btn">Enviar</button>
         </form>
       </div>
 
